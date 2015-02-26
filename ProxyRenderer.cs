e2ed53aa8bc2a3643468ec7a0e1062a5083aaa0c
Joe Cooper <joe@artofbytes.com>
2015-02-26 14:02:35 +0100

    First commit – incomplete

---
+++ ProxyRenderer.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+public class ProxyRenderer : MonoBehaviour
+{
+	public Material[] materials;
+
+	private readonly ProxyRendererObjectPool<CanvasRenderer> canvasRendererPool;
+	private readonly ChangeMonitor monitor;
+
+	private readonly IDictionary<Material,CanvasRenderer> canvasRenderersByMaterial;
+
+	private MeshFilter meshFilter;
+	private MeshRenderer meshRenderer;
+
+	public ProxyRenderer()
+	{
+		canvasRendererPool = new ProxyRendererObjectPool<CanvasRenderer>(CreateCanvasRenderer, ProxyRenderer.DestroyCanvasRenderer, 8);
+		monitor = new ChangeMonitor(true);
+	}
+	
+	void Start() {
+		meshFilter = GetComponent<MeshFilter>();
+
+		monitor.Add(() => IsChildOfCanvas);
+		monitor.Add(() => meshFilter.sharedMesh);
+		monitor.Add<Material[]>(() => materials, ChangeMonitor.ArrayEvaluator);
+	}
+	
+	private bool IsChildOfCanvas {
+		get {
+			return GetComponentInParent<Canvas>() != null;
+		}
+	}
+
+	private CanvasRenderer CreateCanvasRenderer()
+	{
+		var go = new GameObject();
+		var goTransform = go.transform;
+		goTransform.parent = this.transform;
+		goTransform.localScale = Vector3.one;
+		goTransform.localPosition = Vector3.zero;
+		goTransform.localRotation = Quaternion.identity;
+		var canvasRenderer = go.AddComponent<CanvasRenderer>();
+		return canvasRenderer;
+	}
+
+	private static void DestroyCanvasRenderer(CanvasRenderer r)
+	{
+		GameObject.Destroy(r.gameObject);
+	}
+
+	void Update() {
+		if(monitor.Evaluate())
+		{
+			if(IsChildOfCanvas)
+			{
+				if(meshRenderer != null)
+				{
+					GameObject.DestroyImmediate(meshRenderer);
+					meshRenderer = null;
+				}
+			}
+			else {
+				foreach(var canvasRenderer in canvasRenderersByMaterial.Values)
+				{
+					DestroyCanvasRenderer(canvasRenderer);
+				}
+				canvasRenderersByMaterial.Clear();
+
+				meshRenderer = meshRenderer ?? gameObject.AddComponent<MeshRenderer>();
+				meshRenderer.sharedMaterials = materials;
+			}
+		}
+	}
+
+	private void PushState()
+	{
+
+	}
+}
---
+++ ProxyRendererObjectPool.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class ProxyRendererObjectPool<T> {
+
+	private Stack<T> pool = new Stack<T>();
+
+	private readonly Func<T> instantiationAgent;
+	private readonly Action<T> disposalAgent;
+
+	private readonly int capacity;
+	
+	public ProxyRendererObjectPool(Func<T> instantiationAgent)
+	{
+		this.instantiationAgent = instantiationAgent;
+		this.disposalAgent = null;
+		this.capacity = int.MaxValue;
+	}
+
+	public ProxyRendererObjectPool(Func<T> instantiationAgent, Action<T> disposalAgent, int capacity = int.MaxValue)
+	{
+		this.instantiationAgent = instantiationAgent;
+		this.disposalAgent = disposalAgent;
+		this.capacity = capacity;
+	}
+
+	public void Clear()
+	{
+		if(disposalAgent != null) {
+			while(pool.Count > 0)
+			{
+				disposalAgent(pool.Pop());
+			}
+		}
+		else {
+			pool.Clear();
+		}
+	}
+
+	public T GetObject()
+	{
+		if(pool.Count > 0)
+		{
+			return pool.Pop();
+		}
+		else
+		{
+			return instantiationAgent();
+		}
+	}
+
+	public void PutObject(T obj)
+	{
+		if(pool.Count > capacity)
+		{
+			disposalAgent(obj);
+		}
+		else
+		{
+			pool.Push(obj);
+		}
+	}
+}
