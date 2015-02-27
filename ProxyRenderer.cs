dcc1ec63fa2d42f70bd75738c06ca1f971e6a7c2
Joe Cooper <joe@artofbytes.com>
2015-02-27 13:34:16 +0100

    ProxyRenderer is whole and working

--- ProxyRenderer.cs
+++ ProxyRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 [RequireComponent(typeof(MeshFilter))]
 public class ProxyRenderer : MonoBehaviour
@@ -9,13 +10,14 @@ public class ProxyRenderer : MonoBehaviour
 	private readonly ProxyRendererObjectPool<CanvasRenderer> canvasRendererPool;
 	private readonly ChangeMonitor monitor;
 
-	private readonly IDictionary<Material,CanvasRenderer> canvasRenderersByMaterial;
+	private readonly IDictionary<int,CanvasRenderer> canvasRenderersBySubmeshIndex;
 
 	private MeshFilter meshFilter;
 	private MeshRenderer meshRenderer;
 
 	public ProxyRenderer()
 	{
+		canvasRenderersBySubmeshIndex = new Dictionary<int, CanvasRenderer>();
 		canvasRendererPool = new ProxyRendererObjectPool<CanvasRenderer>(CreateCanvasRenderer, ProxyRenderer.DestroyCanvasRenderer, 8);
 		monitor = new ChangeMonitor(true);
 	}
@@ -51,7 +53,7 @@ public class ProxyRenderer : MonoBehaviour
 		GameObject.Destroy(r.gameObject);
 	}
 
-	void Update() {
+	void LateUpdate() {
 		if(monitor.Evaluate())
 		{
 			if(IsChildOfCanvas)
@@ -61,22 +63,97 @@ public class ProxyRenderer : MonoBehaviour
 					GameObject.DestroyImmediate(meshRenderer);
 					meshRenderer = null;
 				}
+
+				var mesh = meshFilter.sharedMesh;
+
+				if(mesh != null && materials.Length > 0)
+				{
+					var positions = mesh.vertices;
+					var colors = mesh.colors32;
+					var coords = mesh.uv;
+					var coords2 = mesh.uv2;
+					var normals = mesh.normals;
+					var tangents = mesh.tangents;
+
+					var defaultColor = new Color32(255,255,255,255);
+					var defaultNormal = Vector3.forward;
+					var defaultCoord = Vector2.zero;
+					var defaultTangent = Vector4.zero;
+
+					for(int i = 0; i < mesh.subMeshCount; i++)
+					{
+						var material = materials[i];
+
+						CanvasRenderer canvasRenderer;
+
+						if(canvasRenderersBySubmeshIndex.ContainsKey(i))
+						{
+							canvasRenderer = canvasRenderersBySubmeshIndex[i];
+						}
+						else {
+							canvasRenderersBySubmeshIndex[i] = canvasRenderer = canvasRendererPool.GetObject();
+						}
+
+						var triangleIndices = mesh.GetTriangles(i);
+
+						var triangleCount = triangleIndices.Length / 3;
+						var sourceIndices = new int[triangleCount * 4];
+
+						for(int j = 0; j < triangleCount; j++)
+						{
+							sourceIndices[j * 4] = triangleIndices[j * 3];
+							sourceIndices[j * 4 + 1] = triangleIndices[j * 3];
+							sourceIndices[j * 4 + 2] = triangleIndices[j * 3 + 1];
+							sourceIndices[j * 4 + 3] = triangleIndices[j * 3 + 2];
+						}
+
+						var uiVertices = new UIVertex[sourceIndices.Length];
+
+						for(int j = 0; j < sourceIndices.Length; j++)
+						{
+							var sourceIndex = sourceIndices[j];
+							uiVertices[j] = new UIVertex {
+								position = positions[sourceIndex],
+								color = sourceIndex < colors.Length ? colors[sourceIndex] : defaultColor,
+								normal = sourceIndex < normals.Length ? normals[sourceIndex] : defaultNormal,
+								tangent = sourceIndex < tangents.Length ? tangents[sourceIndex] : defaultTangent,
+								uv0 = sourceIndex < coords.Length ? coords[sourceIndex] : defaultCoord,
+								uv1 = sourceIndex < coords2.Length ? coords2[sourceIndex] : defaultCoord
+							};
+						}
+
+						canvasRenderer.SetMaterial(material, null);
+						canvasRenderer.SetVertices(uiVertices, uiVertices.Length);
+						canvasRenderer.SetColor(Color.white);
+						canvasRenderer.SetAlpha(1f);
+						canvasRenderer.gameObject.SetActive(true);
+					}
+				}
+
+				var subMeshCount = mesh != null ? mesh.subMeshCount : 0;
+
+				if(canvasRenderersBySubmeshIndex.Count > mesh.subMeshCount)
+				{
+					var oldKeys = from key in canvasRenderersBySubmeshIndex.Keys where key >= subMeshCount select key;
+					foreach(var key in oldKeys)
+					{
+						var canvasRenderer = canvasRenderersBySubmeshIndex[key];
+						canvasRenderersBySubmeshIndex.Remove(key);
+						canvasRenderer.gameObject.SetActive(false);
+						canvasRendererPool.PutObject(canvasRenderer);
+					}
+				}
 			}
 			else {
-				foreach(var canvasRenderer in canvasRenderersByMaterial.Values)
+				foreach(var canvasRenderer in canvasRenderersBySubmeshIndex.Values)
 				{
 					DestroyCanvasRenderer(canvasRenderer);
 				}
-				canvasRenderersByMaterial.Clear();
+				canvasRenderersBySubmeshIndex.Clear();
 
 				meshRenderer = meshRenderer ?? gameObject.AddComponent<MeshRenderer>();
 				meshRenderer.sharedMaterials = materials;
 			}
 		}
-	}
-
-	private void PushState()
-	{
-
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ProxyRenderer
-A Unity3D component that provides an interface to assign geometry, and presents it by controlling either a MeshRenderer or a set of CanvasRenderers, depending on circumstances.
+A Unity3D component that provides a way to present geometry *either* through a CanvasRenderer *or* a MeshRenderer, as appropriate.
+
+ProxyRenderer is attached a component alongside a MeshFilter. It observes the MeshFilter and manages the existence of *either* a MeshRenderer *or* a set of CanvasRenderers.
+
+The transfer of geometry from the Mesh – provided by the filter – to the CanvasRenderers can be computationally expensive as the data must be extracted from the Mesh through its high level interfaces, then generate the interleaved vertex data which the CanvasRenderers expect.
+
+Changes to the set of materials, or submesh count, also involve labor when presenting through CanvasRenderers. Because CanvasRenderers will not coexist attached to the same object, and will not present multiple materials, a set of child objects must be managed.
+
+## Quads
+
+Due to the fact that the CanvasRenderer interface takes only quads, and the Mesh class provides triangles, ProxyRenderer must push triangles *as* quads by doubling up one of the vertices.
+
+This presents two problems.
+
+First, it relies on undefined behavior in Unity3D; triangles are presented as quads by doubling the *first* vertex, which makes an assumption about how Unity will convert the quads *back* into triangles. This means that an internal change in a subsequent Unity release will break this kit and warrant amendment.
+
+Second, this will induce unwarranted computational work at runtime.
+
+## Why?
+
+ProxyRenderer is created as bridge code for Noble Reader (found in the Unity Asset Store), so that the Noble Reader can present in a canvas while remaining compatible with existing configurations.
+
+It is an esoteric thing, but perhaps this isn't the only use for it.
